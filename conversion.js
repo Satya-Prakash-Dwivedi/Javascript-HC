37aafb868b8834dcc209f44809386ec567e2ebed
Satya Prakash Dwivedi <111746703+Satya-Prakash-Dwivedi@users.noreply.github.com>
2024-03-08 06:47:54 +0000

    Added conversion

---
+++ conversion.js
@@ -0,0 +1,30 @@
+let score = "33"
+
+// console.log(typeof score);
+// console.log(typeof(score));
+
+let scoreinnum = Number(score);
+
+// console.log(typeof(scoreinnum));
+// console.log(scoreinnum);
+
+// "33" = 33
+// "33abc" = NaN
+// ture = 1 , false = 0
+
+let isLoggedin = "satya";
+
+let booleanIslogged = Boolean(isLoggedin);
+// console.log(booleanIslogged);
+
+// 1 => true, 0=> false
+// "" => false
+// "satya" => true
+
+let someNumber = 33
+
+let stringnum = String(someNumber)
+
+console.log(typeof stringnum);
+console.log(stringnum)
+
---
+++ datatypes.js
@@ -0,0 +1,26 @@
+"use strict" // treat all JS code as newer version
+
+alert(3+3)
+
+console.log(3+3) // code readability should be high
+
+console.log("hitesh")
+
+let name = "histesh"
+let age = 18
+let isloggedin = false
+let state;
+
+// number => 2 to power 53
+// bigint
+// string => ""
+// boolean => true/false
+// null => standalone value
+// undefined => 
+// symbol => unique
+
+
+// object
+
+console.log(typeof undefined); // undefined
+console.log(typeof null); // object
